8585077dbc125dc66104cfe89b0e124306028927
Lucas <lucassantosamaral@hotmail.com>
2018-10-24 23:17:38 -0300

    primeiro commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create();
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+gulp.task('server', ['sass', 'uglify'], function(){
+    
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/js/*.js", ['uglify'])
+    gulp.watch("*.html").on('change', bs.reload);
+
+});
+
+gulp.task('sass', function(){
+    pump([
+        gulp.src('src/scss/**/*.scss'),
+        sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError),
+        gulp.dest('dist/css'),
+    ])
+});
+
+gulp.task('uglify', function(){
+    pump([
+        gulp.src('src/js/*.js'),
+        uglify(),
+        gulp.dest('dist/js'),
+    ])
+});
---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    var teste = "fuf";
+
+    alert(teste);
+
+});
