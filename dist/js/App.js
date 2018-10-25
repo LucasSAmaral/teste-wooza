964e3889e1e932aba133323edbb7923493d12b8b
Lucas <lucassantosamaral@hotmail.com>
2018-10-25 00:18:31 -0300

    Configurando rotas

---
+++ dist/js/App.js
@@ -0,0 +1,23 @@
+angular.module('wooza',['ngRoute'])
+    .config(['$routeProvider', '$locationProvider',
+        function($routeProvider, $locationProvider) {
+            
+            $routeProvider
+                .when('/:plataforma', {
+                    templateUrl: 'template/plataforma.html',
+                    controller: 'applicationController'
+                })
+                .when('/:plano', {
+                    templateUrl: 'template/planos.html',
+                    controller: 'applicationController'
+                })
+                .when('/:dados-pessoais', {
+                    templateUrl: 'template/dados-pessoais.html',
+                    controller: 'applicationController'
+                });
+
+                $routeProvider.otherwise({
+                    redirectTo: "/:plataforma"
+                })
+        }
+])
